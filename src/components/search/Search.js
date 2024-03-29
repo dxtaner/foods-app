5b9a9e137032e46f1b850f68155e36c022141c7b
taner <tanerozer16@gmail.com>
2024-03-29 17:20:16 +0300

    Search added

---
+++ src/components/search/Search.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./Search.css";
+
+function Search({ handleSubmit }) {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    handleSubmit(e, search);
+    setSearch("");
+  };
+
+  return (
+    <form className="search-form" onSubmit={onSubmit}>
+      <input
+        name="search"
+        id="search"
+        placeholder="Search Recipes"
+        value={search}
+        onChange={handleChange}
+      />
+      <button className="recipe-button" type="submit">
+        Search recipes
+      </button>
+    </form>
+  );
+}
+
+export default Search;
